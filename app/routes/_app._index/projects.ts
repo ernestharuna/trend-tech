8d3b0a29950e71754f38b5045b32163fe431f1ab
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2025-07-07 15:45:57 +0100

    Update project images: Comment out image paths and set placeholder for improved maintainability

--- app/routes/_app._index/projects.ts
+++ app/routes/_app._index/projects.ts
@@ -2,17 +2,20 @@ export const PROJECTS = [
     {
         name: "Johns Hopkins Public Health Initiative",
         tags: "Empowered outreach and health education through mobile-based tools and data systems.",
-        image: "/projects/image.png"
+        // image: "/projects/image.png"
+        image: "#"
     },
 
     {
         name: "MercyCORPS LTD/GTE",
         tags: "Developed a secure Document Archiving and Retrieval System for Mercy Corps' ENGINE program, enabling efficient storage, organization, and remote access to project documents for improved visibility and operational efficiency.",
-        image: "/images/mercy-corps.jpg"
+        // image: "/images/mercy-corps.jpg"
+        image: "#"
     },
     {
         name: "IFAD/NDDC/LIFE-ND",
         tags: "Built a web-based platform to manage incubator and incubatee data, value chain mentorship, and track fund disbursement",
-        image: "/images/ifad-logo.png"
+        // image: "/images/ifad-logo.png"
+        image: "#"
     },
 ]
